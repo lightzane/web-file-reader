e26d9a3410d9ef2332d9d81159b2ed0acb00e266
lightzane <bhest.pat@gmail.com>
2022-02-02 14:26:02 +0800

    fix not reading when uploading same file name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,23 @@ export class AppComponent {
   fileContent: string;
   type: string;
 
-  onFileSelect(event: Event): void {
+  onFileSelect(event: Event, fileInput: HTMLInputElement): void {
 
     const { target } = event;
     const { files } = target as HTMLInputElement;
 
     this.file = files[0];
-    this.type = this.file.name.match(/\.csv$/i) ? 'csv' 
-              : this.file.name.match(/\.json$/i) ? 'json' 
-              : this.file.name.match(/\.txt$/i) ? 'text' 
-              : '';
+    this.type = this.file.name.match(/\.csv$/i) ? 'csv'
+      : this.file.name.match(/\.json$/i) ? 'json'
+        : this.file.name.match(/\.txt$/i) ? 'text'
+          : '';
 
     const fileReader = new FileReader();
     fileReader.readAsText(this.file, 'utf-8');
 
     fileReader.onload = ((fileLoadedEvent) => {
       this.fileContent = fileLoadedEvent.target.result as string;
+      fileInput.value = ''; // enable re-read when uploading same file name
     });
 
   }
